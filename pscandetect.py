9b6fdab3e0c66aed95302e225fbe873a76e19064
n1cfury <fukugetin@gmail.com>
2017-04-15 18:31:21 -0700

    adding scanners

---
+++ pscandetect.py
@@ -0,0 +1,16 @@
+
+
+def banner():
+	print "port scanner detector p59"
+
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
---
+++ websrvscan.py
@@ -0,0 +1,15 @@
+
+
+def banner():
+	print "web server scanner p93"
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
