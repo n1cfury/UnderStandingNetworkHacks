b076af166eb75b60c5d86052486b34c1f9a6be95
n1cfury <fukugetin@gmail.com>
2017-04-15 18:27:17 -0700

    adding sniffing tools

---
+++ sniffer.py
@@ -0,0 +1,12 @@
+
+
+def banner():
+	print "TCP sniffer p48"
+
+def usage():
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
---
+++ sniffinder.py
@@ -0,0 +1,16 @@
+
+
+def banner():
+	print "sniffer detector p 54"
+
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
