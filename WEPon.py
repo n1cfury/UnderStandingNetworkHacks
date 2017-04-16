3b32f0623b8a4ebb126c7156aac4f0e3f33fc1fd
n1cfury <fukugetin@gmail.com>
2017-04-15 18:27:52 -0700

    adding WPA and WEP-lol tools

---
+++ WEPon.py
@@ -0,0 +1,15 @@
+
+
+def banner():
+	print "WEP cracker p 121"
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
---
+++ WPAon.py
@@ -0,0 +1,15 @@
+
+
+def banner():
+	print "WPA cracker p 123"
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
