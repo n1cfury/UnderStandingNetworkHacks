a12ef09fd3d38f5547cb05271fb16c5f6df91161
n1cfury <fukugetin@gmail.com>
2017-04-15 18:26:53 -0700

    adding DNS tools

---
+++ dnsmapper.py
@@ -0,0 +1,16 @@
+
+
+def banner():
+	print "dns mapper p76"
+
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
---
+++ dnsspoof.py
@@ -0,0 +1,16 @@
+
+
+def banner():
+	print "dns spoofing p80"
+
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
