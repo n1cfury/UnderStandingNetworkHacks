3f725f5d1e7be0c02bea7c5d15495861def30543
n1cfury <fukugetin@gmail.com>
2017-04-15 18:29:48 -0700

    adding more sniffers

---
+++ probereqsniff.py
@@ -0,0 +1,15 @@
+
+
+def banner():
+	print "probe request sniff p 119"
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
---
+++ sslsniff.py
@@ -0,0 +1,15 @@
+
+
+def banner():
+	print "SSL Sniff p105"
+def usage():
+
+def handle_packet():
+
+
+
+def main():
+	banner()
+
+if __name__ == '__main__':
+	main()
